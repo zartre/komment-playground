b0461f4ccdd18d61e73530770f70f767b2957569
Nathan Yiangsuppanontr <git@zartre.com>
2025-08-16 23:01:36 +0700

    refactor: move variable out of func

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,18 +2,18 @@ import { Hono } from 'hono';
 import bookHandler from './book';
 import pingHandler from './ping';
 
-export const registerRoutes = (app: Hono<{ Bindings: Env }>) => {
-	const routes = [
-		{
-			path: '/ping',
-			handler: pingHandler,
-		},
-		{
-			path: '/books',
-			handler: bookHandler,
-		},
-	];
+const routes = [
+	{
+		path: '/ping',
+		handler: pingHandler,
+	},
+	{
+		path: '/books',
+		handler: bookHandler,
+	},
+];
 
+export const registerRoutes = (app: Hono<{ Bindings: Env }>) => {
 	routes.forEach((route) => {
 		app.route(route.path, route.handler);
 	});
